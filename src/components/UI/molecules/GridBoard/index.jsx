ef29f095731ea2b57f5958b962cdf9154ba4d233
si-saaref <achmadsyarif241@gmail.com>
2022-12-23 13:03:11 +0700

    feat: create tile component

--- src/components/UI/molecules/GridBoard/index.jsx
+++ src/components/UI/molecules/GridBoard/index.jsx
@@ -1,15 +1,17 @@
+import Tile from '../../atoms/Tile';
+
 export default function GridBoard({ grid }) {
 	return (
 		<>
 			<table>
 				<tbody>
-					{/* {grid.map((rowVal, row) => (
+					{grid.map((rowVal, row) => (
 						<tr key={`r-${row}`}>
 							{rowVal.map((colVal, col) => {
 								return <Tile id={`item-${row}-${col}`} type={colVal} key={`el-${row}${col}`} />;
 							})}
 						</tr>
-					))} */}
+					))}
 				</tbody>
 			</table>
 		</>
